da5056dc8a73d320a708f36d90cece4dee2f1adc
Artem Fivko <a.fivko@gmail.com>
2025-02-13 14:40:41 +0000

    Anyuta

--- src/App.jsx
+++ src/App.jsx
@@ -76,15 +76,14 @@ export default function Page() {
 
 const Footer = () => {
   return (
-    <a
+    <a 
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
+      href="/assets/photo.jpg" 
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
+      Made with {" "}
+      <span role="img" aria-label="heart">❤️</span>
     </a>
   );
 };
