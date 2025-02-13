b786035e8efb74f3b7399261e33f677c9e8b95e2
Artem Fivko <a.fivko@gmail.com>
2025-02-13 12:35:50 +0000

    Anyta

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,10 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "Ні",
+      "Точно?",
+      "Дійсно впевнена?",
+      
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -43,7 +28,7 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            Окееей!!!
           </div>
         </>
       ) : (
@@ -61,7 +46,7 @@ export default function Page() {
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Анюточка давай почнемо зустрічатися і подивимось серіал сьогодні?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -69,13 +54,13 @@ export default function Page() {
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Так
             </button>
             <button
               onClick={handleNoClick}
               className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Ні" : getNoButtonText()}
             </button>
           </div>
         </>
