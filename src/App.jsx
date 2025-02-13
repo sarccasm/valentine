b0709303db7fd531440d87995f5ed44585751b9a
Artem Fivko <a.fivko@gmail.com>
2025-02-13 14:58:46 +0000

    Виправлений шлях до фото

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
   return (
     <a 
     className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-    href="/valentine/public/photo.jpg"
+    href="/valentine/photo.jpg"
     target="_blank"
     rel="noopener noreferrer">
     Made with ❤️
